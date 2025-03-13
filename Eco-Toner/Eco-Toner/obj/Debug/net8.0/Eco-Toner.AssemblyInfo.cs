158b5f55e8dd5efa5ead05b9c32d2f1717dd5429
Diego <diegocosillo@gmail.com>
2025-03-12 19:56:05 -0600

    Interfaz de login

--- Eco-Toner/Eco-Toner/obj/Debug/net8.0/Eco-Toner.AssemblyInfo.cs
+++ Eco-Toner/Eco-Toner/obj/Debug/net8.0/Eco-Toner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eco-Toner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbda1a4497af10cc3d897c7a1aa63e58f94162f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e14e0dc131d1711ac5f66cac390eed4e171398b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eco-Toner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eco-Toner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
