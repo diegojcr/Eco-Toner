3e036c11340ae7fd2bf69fb34478c42dabf8d26c
Sebastian <uecuevasu@gmail.com>
2025-03-14 12:16:25 -0600

    FORMULARIO CREACION Y ASIGNACION DE TICKETS

---
+++ Eco-Toner/Eco-Toner/Controllers/FormularioTicketController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eco_Toner.Controllers
+{
+    public class FormularioTicketController : Controller
+    {
+        public IActionResult FormularioTicket()
+        {
+            return View();
+        }
+    }
+}
