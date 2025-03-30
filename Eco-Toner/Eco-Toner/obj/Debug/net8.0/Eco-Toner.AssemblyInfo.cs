17bda8de2cde732364ebfb7471b21d636d8baf56
Sebastian <uecuevasu@gmail.com>
2025-03-30 00:33:09 -0600

    PRUEBAS FINALES

--- Eco-Toner/Eco-Toner/obj/Debug/net8.0/Eco-Toner.AssemblyInfo.cs
+++ Eco-Toner/Eco-Toner/obj/Debug/net8.0/Eco-Toner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eco-Toner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+490ac916651e205ca982768557efb296d7938387")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eco-Toner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eco-Toner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
